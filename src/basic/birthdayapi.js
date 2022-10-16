f4dbb26bc0cda778ff67c153c9858de7fe21e6ea
demon-ora <softwareselling26@gmail.com>
2022-10-16 15:50:04 +0545

    use effect with title increament and second parameter

---
+++ src/basic/birthdayapi.js
@@ -0,0 +1,30 @@
+export const data = [
+ {
+  id: 1,
+  img:
+ 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  title: 'kirito',
+  age:20,
+ },
+ {
+  id: 2,
+  img:
+   'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  title: 'gut',
+  age:30,
+ },
+ {
+  id: 3,
+  img:
+   'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  title: 'ken kanakei',
+age:14,
+ },
+ {
+   id: 4,
+  img:
+   'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  title: 'undertaker',
+age:12,
+ },
+];
