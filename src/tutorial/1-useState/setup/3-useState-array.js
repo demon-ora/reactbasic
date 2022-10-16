3520f67586daac4725ba7f6b108ce707e019f01a
demon-ora <softwareselling26@gmail.com>
2022-10-16 13:22:27 +0545

    upload some react usestate baisc to delete data

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+import React,{useState} from 'react';
+import { data } from '../../../data'; 
+import '../../../index.css'
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const[people,setpeople] = useState(data);
+    
+const clearone = (id) => {
+ const newpers = people.filter((newper)=>
+newper.id !== id
+ )
+ setpeople(newpers)
+}
+
+  return<> 
+     {
+      people.map((person)=>{
+       const {id,name}=person;
+       return(
+        <div className='item'>{name}
+          <button onClick={()=>clearone(id)}>clearone</button>
+        </div>
+      
+       );
+      })
+     }
+     <button onClick={()=> setpeople([])}>clearall</button>
+  </>
 };
 
 export default UseStateArray;
