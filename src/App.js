ae4ed00e4c2121b8039fcf2a721110cba3edc509
demon-ora <softwareselling26@gmail.com>
2022-10-16 18:59:37 +0545

    use effect for api fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import  ErrorExample from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import  ErrorExample from './tutorial/2-useEffect/3-useEffect-fetch-data'
+
 function App() {
   return (
     <div className='container'>
