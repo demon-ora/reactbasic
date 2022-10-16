1ccbe4024a11c1c41cdec425bc7f5b8e8c4785e6
demon-ora <softwareselling26@gmail.com>
2022-10-16 13:37:28 +0545

    upload some react usestate obj to add something in usestate using obj or changing something

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+ 
+  const [obj,setobj] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+    
+const changeMessage = () =>{  
+  setobj({...obj,me:"helloworld"});
+}
+
+  return <>
+    <h3>{obj.name}</h3>
+      <h3>{obj.age}</h3>
+      <h4>{obj.message}</h4>
+        <h4>{obj.me}</h4>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+  </>
 };
 
 export default UseStateObject;
