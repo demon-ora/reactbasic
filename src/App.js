fde881330d65b752e6f5f25e3e799fb0f448160b
demon-ora <softwareselling26@gmail.com>
2022-10-16 14:54:55 +0545

    upload some react usestate with counter and some function counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import  ErrorExample from './tutorial/1-useState/setup/4-useState-object'
+import  ErrorExample from './tutorial/1-useState/setup/5-useState-counter'
 function App() {
   return (
     <div className='container'>
